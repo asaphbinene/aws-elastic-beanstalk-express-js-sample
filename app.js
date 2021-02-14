0c53bc224f700d56ba52584196b20c0d204830ef
KAT Binene <asaphbinene@outlook.com>
2021-02-14 03:47:07 +0200

    Full pipeline tested

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to KATBIN.COM Cloud!'));
+app.get('/', (req, res) => res.send('Welcome to KATBIN.COM Cloud the deploy read for publishing!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
